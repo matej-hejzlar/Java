7cf2c936c6beb92c20447ab746f6d1d40e82eeab
matej-hejzlar <67500456+matej-hejzlar@users.noreply.github.com>
2021-10-15 18:48:03 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,59 @@
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+
+		/*
+		 * while(true) { System.out.print("Nigga you gay"); scanner.nextLine(); }
+		 * 
+		 * 	String[] cars = new String[3]; 
+		 *	cars[0] = "ne";
+		 *	cars[1] = "ano";
+		 *		
+		 *	System.out.println(cars[0]);
+		 *
+		 * 
+		 * 
+		 * String day = scanner.nextLine();
+		 * 
+		 * switch(day) { case "Sunday": System.out.println(day+" is weekend day");
+		 * break; case "Monday": System.out.println(day+" is not a weekend day"); break;
+		 * default: System.out.println("This is not a valid option"); }
+		 * 
+		 * System.out.print("Input true or false: "); boolean actionTrueOrFalse =
+		 * scanner.nextBoolean();
+		 * 
+		 * if(actionTrueOrFalse == true) {
+		 * System.out.println("Action = "+actionTrueOrFalse); }else if(actionTrueOrFalse
+		 * == false) { System.out.println("Action = "+actionTrueOrFalse); }
+		 * 
+		 * Scanner scanner = new Scanner(System.in);
+		 * 
+		 * System.out.print("Input X: "); double x = scanner.nextDouble();
+		 * scanner.nextLine(); System.out.print("Input Y: "); double y =
+		 * scanner.nextDouble();
+		 * 
+		 * double z = Math.max(x, y);
+		 * 
+		 * System.out.println(z);
+		 * 
+		 * String name = JOptionPane.showInputDialog("What's you name?"); int age =
+		 * Integer.parseInt(JOptionPane.showInputDialog("How old are you?")); String
+		 * favoriteFood = JOptionPane.showInputDialog("What's your favorite food?");
+		 * String details =
+		 * "Hello "+name+", you are "+age+" years old and your favorite food is "
+		 * +favoriteFood;
+		 * 
+		 * System.out.println(details); JOptionPane.showMessageDialog(null, details);
+		 * int friends = 2; friends = friends 2; Scanner scanner = new
+		 * Scanner(System.in); System.out.println("What is your name: "); String name =
+		 * scanner.nextLine(); System.out.print("Hello "+name);
+		 */
+		
+	}
+		
+}
